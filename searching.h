fe3cf451487c0d97f5dd95652076229791b020c6
yigit <yigit@DESKTOP-M2O195Q>
2025-11-08 23:52:59 +0100

    Proje dosyası ekle.

---
+++ searching.h
@@ -0,0 +1,40 @@
+#include <array>
+#include <vector>
+
+template <typename T, unsigned long N>
+int binarySearchFirst(std::array<T, N> arr, T target) {
+    int left = 0, right = static_cast<int>(N) - 1, ans = -1;
+    while (left <= right) {
+        int middle = left + (right - left) / 2;
+        if (arr[middle] == target) {
+            ans = middle;
+            right = middle - 1;
+        }
+        else if (target < arr[middle]) {
+            right = middle - 1;
+        }
+        else {
+            left = middle + 1;
+        }
+    }
+    return ans;
+}
+
+template <typename T>
+int binarySearchFirst(std::vector<T> arr, T target) {
+    int left = 0, right = static_cast<int>(arr.size()) - 1, ans = -1;
+    while (left <= right) {
+        int middle = left + (right - left) / 2;
+        if (arr[middle] == target) {
+            ans = middle;
+            right = middle - 1;
+        }
+        else if (target < arr[middle]) {
+            right = middle - 1;
+        }
+        else {
+            left = middle + 1;
+        }
+    }
+    return ans;
+}
---
+++ task 6kas.cpp
@@ -0,0 +1,10 @@
+﻿#include "Searching.h"
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> a{ 2,4,7,9,12,15,15,15,15,29,70,80,80 };
+    int result = binarySearchFirst(a, 15);
+    std::cout << "First occurrence index: " << result << std::endl;
+    return 0;
+}
